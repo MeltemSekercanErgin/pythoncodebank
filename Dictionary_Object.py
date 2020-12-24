7e4421bed2a0ba3ce2fad417e70a5f94d792f4e1
Taki <takicakb@gmail.com>
2020-12-24 19:37:20 +0300

    Update Dictionary_Object.py

--- Dictionary_Object.py
+++ Dictionary_Object.py
@@ -7,7 +7,15 @@ Created on Thu Dec 24 19:30:08 2020
 # Boş dictionary
 a = {}
 
+
+
 # Dictionary nesnelerinde key alanı int yada string olabilir.
 # Value değeri istenilen tipde olabilir.
 
-a = {3: "bla"}
+#Geçerli Dictionary tanımları
+a = {}
+
+a = {3: "bla"}  # key=3  value="bla"
+
+# Dictionary nesnesindeki 3 keyinin karşılığı value değerini değiştirme
+a[3]="blabla"
