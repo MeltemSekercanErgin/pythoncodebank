88271874227c64023be78ea244ec37f11344f9cd
MeltemSekercanErgin <76524882+MeltemSekercanErgin@users.noreply.github.com>
2021-02-17 16:15:54 +0300

    Basit Grafik Eklendi

--- Tefas/fon_karsilastirma.py
+++ Tefas/fon_karsilastirma.py
@@ -12,6 +12,11 @@ from PyQt5.QtWidgets import *
 
 import sys
 
+import matplotlib.pyplot as plt
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas 
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar 
+
 
 class pandasModel(QAbstractTableModel):
 
@@ -53,6 +58,8 @@ class Ui_Form(object):
         self.lblDosya.setGeometry(5, 5, 345, 20)
         self.lblDosya.setText("Dosya İsmi")
         
+        
+        
         self.cmbSira = QComboBox(Form)
         self.cmbSira.setGeometry(5, 35, 170, 20)
         self.cmbSira.addItem ("Sıralama Kolonu")
@@ -69,6 +76,21 @@ class Ui_Form(object):
         self.optDESC.setGeometry(260, 35, 80, 20)
         self.optDESC.setText("Desc")
         
+        
+        self.cmbGrX = QComboBox(Form)
+        self.cmbGrX.setGeometry(5, 65, 170, 20)
+        self.cmbGrX.addItem ("Graph X Value")
+        
+        self.cmbGrY = QComboBox(Form)
+        self.cmbGrY.setGeometry(180, 65, 170, 20)
+        self.cmbGrY.addItem ("Graph Y Value")
+        
+        self.btnGr = QPushButton(Form)
+        self.btnGr.setGeometry(350, 65, 40, 20)
+        self.btnGr.setText("Çiz")
+        
+        
+        
         self.view = QTableView(Form)
         self.view.setGeometry(380, 5, 950, 550)
         
@@ -83,15 +105,28 @@ class Ui_Form(object):
        
         
         self.btnSira.clicked.connect(lambda : self.Sirala())
+        self.btnGr.clicked.connect(lambda : self.Grafik())
 
-   
+        
+        
+        
+        
     def veriyukle(self, df):
         model = pandasModel(df)
         self.view.setModel(model )
         self.view.setSortingEnabled(True)
         
+        self.cmbSira.clear()
+        self.cmbSira.addItem ("Sıralama Kolonu")
         self.cmbSira.addItems(df.columns)
            
+        self.cmbGrX.clear()
+        self.cmbGrX.addItem ("Graph X Value")
+        self.cmbGrX.addItems(df.columns)
+        
+        self.cmbGrY.clear()
+        self.cmbGrY.addItem ("Graph Y Value")
+        self.cmbGrY.addItems(df.columns)
         
     def DosyaSec(self):
         dosya = QFileDialog.getOpenFileName( caption ="Dosya Seç", filter="Excell dosyası (*.xlsx)")[0]
@@ -105,8 +140,7 @@ class Ui_Form(object):
         df = pd.read_excel(dosya)
         df_copy = df
         
-        self.cmbSira.clear()
-        self.cmbSira.addItem ("Sıralama Kolonu")
+        
         
         self.veriyukle(df)
         
@@ -117,6 +151,29 @@ class Ui_Form(object):
             global df
             df = df.sort_values(self.cmbSira.currentText(), ascending=self.optASC.isChecked(), axis=0)   
             self.veriyukle(df)
+            
+    def Grafik (self):
+        
+        if self.cmbGrX.currentIndex() != 0 and self.cmbGrY.currentIndex() != 0 : 
+            x = df[self.cmbGrX.currentText()]
+            y = df[self.cmbGrY.currentText()]
+            """
+            df.plot(x = self.cmbGrX.currentText(), y = self.cmbGrY.currentText())
+            """
+            
+            self.figure = plt.figure() 
+            
+            self.canvas = FigureCanvas(self.figure)
+        
+            self.figure.clear()
+            ax =  self.figure.add_subplot(111) 
+            
+            ax.plot(x,y)
+            ax.set_title (self.cmbGrX.currentText() + " & " + self.cmbGrY.currentText())
+            ax.set_xlabel(self.cmbGrX.currentText())
+            ax.set_ylabel(self.cmbGrY.currentText())
+            
+            self.canvas.draw()
             
 if __name__ == "__main__":
     import sys
