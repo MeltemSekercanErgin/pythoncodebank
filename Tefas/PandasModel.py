fefa9a91feba5350aef03dd80ec6b6d836c375a4
MeltemSekercanErgin <76524882+MeltemSekercanErgin@users.noreply.github.com>
2021-02-17 14:58:13 +0300

    Tefas Procesi :)
    Yeni Başladık Bunda Bir Şey Yok Deme ;)

---
+++ Tefas/PandasModel.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 12:50:03 2021
+
+@author: SS
+"""
+
+class pandasModel(QAbstractTableModel):
+
+    def __init__(self, data):
+        QAbstractTableModel.__init__(self)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return self._data.shape[0]
+
+    def columnCount(self, parnet=None):
+        return self._data.shape[1]
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return str(self._data.iloc[index.row(), index.column()])
+        return None
+
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self._data.columns[col]
+        return None
