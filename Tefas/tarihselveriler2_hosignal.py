feee1b21c6bdad31c9ee581695fb47c96f7d3c7b
MeltemSekercanErgin <76524882+MeltemSekercanErgin@users.noreply.github.com>
2021-02-26 14:58:17 +0300

    EMA

---
+++ Tefas/tarihselveriler2_hosignal.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 11:51:31 2021
+
+@author: SS
+"""
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+df = pd.read_csv("TarihselVeriler.csv")
+
+
+df = df.iloc[len(df)//2:]
+
+
+df.dropna(inplace=True)
+
+
+for i in df["FON KODU"].unique():
+    
+    tmpdf = df[df["FON KODU"] == "AFT" ]
+    #tmpdf["ho5"] = tmpdf["FİYAT"].ewm(span =10).mean()
+    tmpdf["ho14"] = tmpdf["FİYAT"].ewm(span =30).mean()
+    tmpdf["ho21"] = tmpdf["FİYAT"].ewm(span =50).mean()
+    tmpdf["ho30"] = tmpdf["FİYAT"].ewm(span =200).mean()
+    
+    
+    tmpdf = pd.melt(tmpdf, id_vars=['TARİH'], value_vars=['FİYAT', 'ho14', 'ho21', 'ho30'])
+    #tmpdf = pd.melt(tmpdf, id_vars=['TARİH'], value_vars=[ 'ho5', 'ho14', 'ho21', 'ho30'])
+    
+    sns.lineplot(x ="TARİH", y = "value", hue="variable", data=tmpdf).set_title("AFT")
+       
+    plt.show()
+    break
+   
