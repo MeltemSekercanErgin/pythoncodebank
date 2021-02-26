b7570fb1bd82b7ddc4e3cb10dc16ad3069f2e5ca
MeltemSekercanErgin <76524882+MeltemSekercanErgin@users.noreply.github.com>
2021-02-26 15:37:57 +0300

    ma

--- Tefas/tarihselveriler2_hosignal.py
+++ Tefas/tarihselveriler2_hosignal.py
@@ -17,27 +17,31 @@ df = pd.concat([df,df2])"""
 df = pd.read_csv("TarihselVeriler.csv")
 
 df['TARİH'] = pd.to_datetime(df['TARİH'], format="%Y-%m-%d")
-
-
-
-
 df.dropna(inplace=True)
+
+
+df = df.iloc[len(df)//2:]
+
+
 
 
 for i in df["FON KODU"].unique():
     
-    tmpdf = df[df["FON KODU"] == i ]
+    tmpdf = df[df["FON KODU"] == "IPJ" ]
     #tmpdf["ho5"] = tmpdf["FİYAT"].ewm(span =10).mean()
-    tmpdf["ho14"] = tmpdf["FİYAT"].ewm(span =30).mean()
+    """tmpdf["ho14"] = tmpdf["FİYAT"].ewm(span =30).mean()
     tmpdf["ho21"] = tmpdf["FİYAT"].ewm(span =50).mean()
-    tmpdf["ho30"] = tmpdf["FİYAT"].ewm(span =200).mean()
+    tmpdf["ho30"] = tmpdf["FİYAT"].ewm(span =200).mean()"""
+    tmpdf["ho14"] = tmpdf["FİYAT"].rolling(window =30).mean()
+    tmpdf["ho21"] = tmpdf["FİYAT"].rolling(window =50).mean()
+    tmpdf["ho30"] = tmpdf["FİYAT"].rolling(window =200).mean()
     
     
     tmpdf = pd.melt(tmpdf, id_vars=['TARİH'], value_vars=['FİYAT', 'ho14', 'ho21', 'ho30'])
     #tmpdf = pd.melt(tmpdf, id_vars=['TARİH'], value_vars=[ 'ho5', 'ho14', 'ho21', 'ho30'])
     
-    sns.lineplot(x ="TARİH", y = "value", hue="variable", data=tmpdf).set_title(i)
+    sns.lineplot(x ="TARİH", y = "value", hue="variable", data=tmpdf).set_title("IPJ")
        
     plt.show()
-    
+    break
    
