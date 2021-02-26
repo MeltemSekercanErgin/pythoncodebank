04b156673b1399c2edaa54b15064cf4d58449ced
MeltemSekercanErgin <76524882+MeltemSekercanErgin@users.noreply.github.com>
2021-02-26 19:37:12 +0300

    tüm fonlar için çalışıyor

--- Tefas/tarihselveriler2_hosignal.py
+++ Tefas/tarihselveriler2_hosignal.py
@@ -20,15 +20,18 @@ df['TARİH'] = pd.to_datetime(df['TARİH'], format="%Y-%m-%d")
 df.dropna(inplace=True)
 
 
-df = df.iloc[len(df)//2:]
 
 
 
 
 for i in df["FON KODU"].unique():
     
-    tmpdf = df[df["FON KODU"] == "IPJ" ]
+    tmpdf = df[df["FON KODU"] == i ]
     #tmpdf["ho5"] = tmpdf["FİYAT"].ewm(span =10).mean()
+    tmpdf['TARİH'] = pd.to_datetime(tmpdf['TARİH'], format="%Y-%m-%d")
+    tmpdf.sort_values("TARİH", inplace=True)
+    
+    
     """tmpdf["ho14"] = tmpdf["FİYAT"].ewm(span =30).mean()
     tmpdf["ho21"] = tmpdf["FİYAT"].ewm(span =50).mean()
     tmpdf["ho30"] = tmpdf["FİYAT"].ewm(span =200).mean()"""
@@ -40,8 +43,8 @@ for i in df["FON KODU"].unique():
     tmpdf = pd.melt(tmpdf, id_vars=['TARİH'], value_vars=['FİYAT', 'ho14', 'ho21', 'ho30'])
     #tmpdf = pd.melt(tmpdf, id_vars=['TARİH'], value_vars=[ 'ho5', 'ho14', 'ho21', 'ho30'])
     
-    sns.lineplot(x ="TARİH", y = "value", hue="variable", data=tmpdf).set_title("IPJ")
+    sns.lineplot(x ="TARİH", y = "value", hue="variable", data=tmpdf).set_title(i)
        
     plt.show()
-    break
+    
    
