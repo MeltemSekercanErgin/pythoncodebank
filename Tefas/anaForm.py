105668bc3ab11f5709d2470f491bf208f8730c97
MeltemSekercanErgin <76524882+MeltemSekercanErgin@users.noreply.github.com>
2021-03-17 12:16:36 +0300

    Hata Yakalama vs
    Hata yakalama kodları decorator içerisine yerleştirildi ve mesajbox göstermesi sağlandı.
    Fon izleme kısmının trendler kısmına bir label yerleştirildi ve trendinin yükseliş düşüş mü olduğu notu gösteriliyor
    HOların veritabanı ve grafikteki isimleri short middle ve long olarak düzeltildi
    Veri birleştirme kısmı formlardan menüye alındı.
    Veri yükleme butonları kaldırıldı. Form yüklenirken otomatik yüklüyor

--- Tefas/anaForm.py
+++ Tefas/anaForm.py
@@ -5,123 +5,133 @@ Created on Wed Mar 10 12:13:32 2021
 @author: Meltem ERGİN :)
 """
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMdiArea, QAction, QMdiSubWindow, QTextEdit
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMdiArea, QAction, QMdiSubWindow, QTextEdit, QMessageBox
 import sys
 import frmFonIzle
 import frmTumFonAnaliz
 from copy import copy
-
+import tarihselveriler as tveri
 
 class MDIWindow(QMainWindow):
 
     count = 0
+    @tveri.hatayakala
     def __init__(self):
-        try :
-            super().__init__()
     
-            self.mdi = QMdiArea()
-            self.setCentralWidget(self.mdi)
-            self.bar = self.menuBar()
-    
-            self.mnuSecenekler = self.bar.addMenu("Dosya")
-            self.mnuSecenekler.addAction("Kapat")
-            self.mnuSecenekler.addSeparator()
-            #mnuSecenekler.addAction("Formları Diz")
+        super().__init__()
+
+        self.mdi = QMdiArea()
+        self.setCentralWidget(self.mdi)
+        self.bar = self.menuBar()
+
+        self.mnuSecenekler = self.bar.addMenu("Dosya")
+        self.mnuSecenekler.addAction("Kapat")
+        self.mnuSecenekler.addSeparator()
+        #mnuSecenekler.addAction("Formları Diz")
+        
+        
+        
+        self.mnuFon = self.bar.addMenu("Fon")
+        self.mnuFon.addAction("Fon İzleme")
+        self.mnuFon.addAction("Tüm Fonlar İzleme")
+        self.mnuFon.addSeparator()
+        self.mnuFon.addAction("Veri Birleştir")
+        
+        self.mnuFon.triggered[QAction].connect(self.WindowTrig)
+        self.mnuSecenekler.triggered[QAction].connect(self.WindowTrig)
+        self.setWindowTitle("MDI Application")
+        
+        
+        global win_subwindow1
+        win_subwindow1 = list()
+        
+        global sub
+        sub = list()
             
-            
-            
-            self.mnuFon = self.bar.addMenu("Fon")
-            self.mnuFon.addAction("Fon İzleme")
-            self.mnuFon.addAction("Tüm Fonlar İzleme")
-            
-            self.mnuFon.triggered[QAction].connect(self.WindowTrig)
-            self.mnuSecenekler.triggered[QAction].connect(self.WindowTrig)
-            self.setWindowTitle("MDI Application")
-            
-            
-            global win_subwindow1
-            win_subwindow1 = list()
-            
-            global sub
-            sub = list()
-            
-        except BaseException as e:
-            
-            print("Bir Hata Oluştu : ", e)
-            
+    """
+    @tveri.hatayakala
     def closeEvent(self, event):
         #print("ana form kapat")
-        event.accept()
         
-    
+        event.accept()"""
+        
+    @tveri.hatayakala
     def WindowTrig(self, p):
-        try :
+   
 
-            if p.text() == "Tüm Fonlar İzleme":
-                
-                MDIWindow.count = MDIWindow.count + 1
-                sub.append( QMdiSubWindow())
-                
-                myForm = copy(frmTumFonAnaliz.Ui_frmTumFonAnaliz())
-                
-                win_subwindow1.append( myForm)
-                win_subwindow1[-1].setupUi(sub[-1])
-                
-                strTitle = "Tüm Fonlar İzleme " + str(MDIWindow.count)
-                sub[-1].setObjectName(strTitle)
-                sub[-1].setWindowTitle(strTitle)
-                
-                
+        if p.text() == "Tüm Fonlar İzleme":
+            
+            MDIWindow.count = MDIWindow.count + 1
+            sub.append( QMdiSubWindow())
+            
+            myForm = copy(frmTumFonAnaliz.Ui_frmTumFonAnaliz())
+            
+            win_subwindow1.append( myForm)
+            win_subwindow1[-1].setupUi(sub[-1])
+            
+            strTitle = "Tüm Fonlar İzleme " + str(MDIWindow.count)
+            sub[-1].setObjectName(strTitle)
+            sub[-1].setWindowTitle(strTitle)
+            
+            
 
-                self.mdi.addSubWindow(sub[-1])
-                sub[-1].move(1, 1)
-                sub[-1].show()
-                
-                myMnu = self.mnuSecenekler.addAction(strTitle)
-                
-                sub[-1].closeEvent = lambda x: self.Kapa(x, strTitle, myMnu)
-                
-            elif p.text() == "Fon İzleme":
-                
-                MDIWindow.count = MDIWindow.count + 1
-                sub.append( QMdiSubWindow())
-                
-                myForm = copy(frmFonIzle.Ui_frmFonIzle())
-                
-                win_subwindow1.append( myForm)
-                win_subwindow1[-1].setupUi(sub[-1])
-                
-                strTitle = "Fon İzleme " + str(MDIWindow.count)
-                sub[-1].setObjectName(strTitle)
-                sub[-1].setWindowTitle(strTitle)
-                
-                
+            self.mdi.addSubWindow(sub[-1])
+            sub[-1].move(1, 1)
+            sub[-1].show()
+            
+            myMnu = self.mnuSecenekler.addAction(strTitle)
+            
+            sub[-1].closeEvent = lambda x: self.Kapa(x, strTitle, myMnu)
+            
+        elif p.text() == "Fon İzleme":
+            
+            MDIWindow.count = MDIWindow.count + 1
+            sub.append( QMdiSubWindow())
+            
+            myForm = copy(frmFonIzle.Ui_frmFonIzle())
+            
+            win_subwindow1.append( myForm)
+            win_subwindow1[-1].setupUi(sub[-1])
+            
+            strTitle = "Fon İzleme " + str(MDIWindow.count)
+            sub[-1].setObjectName(strTitle)
+            sub[-1].setWindowTitle(strTitle)
+            
+            
 
-                self.mdi.addSubWindow(sub[-1])
-                sub[-1].move(1, 1)
-                sub[-1].show()
-                
-                myMnu = self.mnuSecenekler.addAction(strTitle)
-                
-                sub[-1].closeEvent = lambda x: self.Kapa(x, strTitle, myMnu)
-                
-                
-            elif p.text() == "Kapat":
-                
-                self.mdi.close()
-                
-            elif p.text() == "Formları Diz":
-                
-                self.mdi.cascadeSubWindows()
+            self.mdi.addSubWindow(sub[-1])
+            sub[-1].move(1, 1)
+            sub[-1].show()
             
-            else:
-                myForm = self.findChild(QMdiSubWindow, p.text())
-                self.mdi.setActiveSubWindow(myForm)
-                
-        except BaseException as e:
+            myMnu = self.mnuSecenekler.addAction(strTitle)
             
-            print("Bir Hata Oluştu : ", e)
+            sub[-1].closeEvent = lambda x: self.Kapa(x, strTitle, myMnu)
             
+            
+        elif p.text() == "Kapat":
+            
+            self.mdi.close()
+            
+        elif p.text() == "Formları Diz":
+            
+            self.mdi.cascadeSubWindows()
+            
+        elif p.text() == "Veri Birleştir":
+            
+            tveri.veribirlestir()
+            
+            mesaj = QMessageBox()
+            mesaj.setWindowTitle("Bilgi")
+            mesaj.setText("Veri Birleştirme Gerçekleşti.")
+            mesaj.setIcon(QMessageBox.Information)
+            mesaj.exec()
+        
+        else:
+            myForm = self.findChild(QMdiSubWindow, p.text())
+            self.mdi.setActiveSubWindow(myForm)
+            
+        
+    @tveri.hatayakala
     def Kapa(self,event , subName, myMenu):
         
         self.mnuSecenekler.removeAction(myMenu)
--- Tefas/tarihselveriler.py
+++ Tefas/tarihselveriler.py
@@ -10,149 +10,149 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from datetime import date
 from statsmodels.tsa.seasonal import STL
+from PyQt5 import QtWidgets
 
+def hatayakala(function):
+    def wrap_function(*args):
+        try:
+            
+            return function(*args)
+           
+        except BaseException as e:
+            
+            mesaj = QtWidgets.QMessageBox()
+            mesaj.setWindowTitle("Hata Oluştu")
+            mesaj.setText(str(e))
+            mesaj.setIcon(QtWidgets.QMessageBox.Warning)
+            mesaj.exec()
+            
+    return wrap_function
+
+
+@hatayakala
 def veribirlestir():
-    try:
-        df = pd.read_csv("TarihselVeriler_main.csv")
-        df["Tarih"] = pd.to_datetime(df["Tarih"], format="%Y.%m.%d")
-        df.dropna(inplace=True)
-        
-        SonTarih = df["Tarih"].max()
-        
-        
-        dfy = pd.read_csv("TarihselVeriler.csv")
-        dfy["Tarih"] = pd.to_datetime(dfy["Tarih"], format="%d.%m.%Y")
-        dfy.dropna(inplace=True)
-        
-        dfy = dfy[dfy["Tarih"]>SonTarih]
-        
-        
-        if len(dfy)>0:
-            dfy["sil"] = dfy['Fon Adı'].apply(lambda x : ("ÖZEL FON" in x) or ("SERBEST" in x))
-            dfy.drop(dfy[dfy['sil']].index, inplace = True) 
-            dfy.drop('sil', axis=1, inplace=True)
-            
-            df = pd.concat([df, dfy], ignore_index = True)
-            
-            df.to_csv("TarihselVeriler_main.csv", index=False)
-    
-    except BaseException as e:
-            
-            print("Bir Hata Oluştu : ", e)
-        
-    
-    
-def veriYukle():
-    try:
-    
-        df = pd.read_csv("TarihselVeriler_main.csv")
-        df["Tarih"] = pd.to_datetime(df["Tarih"], format="%Y.%m.%d")
-        df.dropna(inplace=True)
-        return df
-    
-    except BaseException as e:
-            
-            print("Bir Hata Oluştu : ", e)
-            
-def fonAnaliz(fon_kod, df, Baslangic, Bitis, HO1 = 30, HO2 = 50, HO3 = 200):
-    
-    try:
-        
-        tmpdf = df[df["Fon Kodu"] == fon_kod ]  
-        tmpdf["Tarih"] = pd.to_datetime(tmpdf["Tarih"], format="%d.%m.%Y")
-        tmpdf.sort_values("Tarih", inplace=True)
-        
-        
-        tmpdf["Fiyat"]= tmpdf["Fiyat"].apply(lambda x: str(x).replace(",","."))
-        tmpdf["Fiyat"] = pd.to_numeric(tmpdf["Fiyat"], downcast="float")
-        
-        
-        tmpdf["ho30"] = tmpdf["Fiyat"].rolling(window =HO1).mean()
-        tmpdf["ho50"] = tmpdf["Fiyat"].rolling(window =HO2).mean()
-        tmpdf["ho200"] = tmpdf["Fiyat"].rolling(window =HO3).mean()
-        
-        tmpdf["SELLs"] =  tmpdf["Fiyat"]<tmpdf["ho30"] 
-        tmpdf["BUYs"] = tmpdf["Fiyat"]>tmpdf["ho200"] 
-        
-        tmpdf["BUY"] =  (tmpdf["BUYs"] == True)  & (tmpdf["BUYs"].rolling(window =2).min() == False)
-        tmpdf["SELL"] =  (tmpdf["SELLs"] == True)  & (tmpdf["SELLs"].rolling(window =2).min() == False)
-        
-        
-        tmpdf["BUY"] = tmpdf["BUY"].astype(int)
-        tmpdf["SELL"] = tmpdf["SELL"].astype(int)
-        
-        tmpdf["UP"] = ( (tmpdf["ho30"] > tmpdf["ho50"] ) & (tmpdf["ho30"] > tmpdf["ho200"] ) )
-        tmpdf["DOWN"] = ( (tmpdf["ho30"] < tmpdf["ho50"] )  & (tmpdf["ho30"] < tmpdf["ho200"] )  )
-        
-        
-       
-    
-        condition1 = tmpdf['Tarih'] >= Baslangic 
-        condition2 = tmpdf['Tarih'] <= Bitis 
-        tmpdf = tmpdf.loc[condition1]
-        tmpdf = tmpdf.loc[condition2]
-        
-        return tmpdf
-    
-    except BaseException as e:
-            
-            print("Bir Hata Oluştu : ", e)
-            
-def fonGrafik(fon_kod, df):
-    try:
-        tmpdf = df.copy()
-        
-        tmpdf["BUY"]=  tmpdf["Fiyat"] * tmpdf["BUY"]
-        tmpdf["SELL"]=  tmpdf["Fiyat"] * tmpdf["SELL"]
-        tmpdf["BUY"].replace(0, np.nan, inplace=True)
-        tmpdf["SELL"].replace(0, np.nan, inplace=True)
-        
-        tmpdf = pd.melt(tmpdf, id_vars=['Tarih', 'BUY','SELL'], value_vars=['Fiyat', 'ho30', 'ho50', 'ho200'])
-        
-        fig, ax = plt.subplots()
-        
-        sns.lineplot(ax = ax, x ="Tarih", y = "BUY",  data=tmpdf, marker="^", color="green")  
-        sns.lineplot(ax = ax, x ="Tarih", y = "SELL",  data=tmpdf, marker="v", color="red")  
-        
-        sns.lineplot(ax = ax,  x ="Tarih", y = "value", hue="variable", data=tmpdf, linewidth=.80)   # 'AES')
-        plt.xticks(rotation='vertical')
-        
-        #today = date.today()
-        #plt.savefig('./plots/'+ fon_kod + today.strftime("%Y%m%d") + '.png') # önce kayıt etmek şart, sonra kayıt edince olmuyor...
-        
-        return fig
-    
-    except BaseException as e:
-            
-            print("Bir Hata Oluştu : ", e)
 
+    df = pd.read_csv("TarihselVeriler_main.csv")
+    df["Tarih"] = pd.to_datetime(df["Tarih"], format="%Y.%m.%d")
+    df.dropna(inplace=True)
+    
+    SonTarih = df["Tarih"].max()
+    
+    
+    dfy = pd.read_csv("TarihselVeriler.csv")
+    dfy["Tarih"] = pd.to_datetime(dfy["Tarih"], format="%d.%m.%Y")
+    dfy.dropna(inplace=True)
+    
+    dfy = dfy[dfy["Tarih"]>SonTarih]
+    
+    
+    if len(dfy)>0:
+        dfy["sil"] = dfy['Fon Adı'].apply(lambda x : ("ÖZEL FON" in x) or ("SERBEST" in x))
+        dfy.drop(dfy[dfy['sil']].index, inplace = True) 
+        dfy.drop('sil', axis=1, inplace=True)
+        
+        df = pd.concat([df, dfy], ignore_index = True)
+        
+        df.to_csv("TarihselVeriler_main.csv", index=False)
+
+@hatayakala   
+def veriYukle():
+
+
+    df = pd.read_csv("TarihselVeriler_main.csv")
+    df["Tarih"] = pd.to_datetime(df["Tarih"], format="%Y.%m.%d")
+    df.dropna(inplace=True)
+    return df
+  
+@hatayakala            
+def fonAnaliz(fon_kod, df, Baslangic, Bitis, HO1 = 30, HO2 = 50, HO3 = 200):
+
+    
+    tmpdf = df[df["Fon Kodu"] == fon_kod ]  
+    tmpdf["Tarih"] = pd.to_datetime(tmpdf["Tarih"], format="%d.%m.%Y")
+    tmpdf.sort_values("Tarih", inplace=True)
+    
+    
+    tmpdf["Fiyat"]= tmpdf["Fiyat"].apply(lambda x: str(x).replace(",","."))
+    tmpdf["Fiyat"] = pd.to_numeric(tmpdf["Fiyat"], downcast="float")
+    
+    
+    tmpdf["ho_short"] = tmpdf["Fiyat"].rolling(window =HO1).mean()
+    tmpdf["ho_middle"] = tmpdf["Fiyat"].rolling(window =HO2).mean()
+    tmpdf["ho_long"] = tmpdf["Fiyat"].rolling(window =HO3).mean()
+    
+    tmpdf["SELLs"] =  tmpdf["Fiyat"]<tmpdf["ho_short"] 
+    tmpdf["BUYs"] = tmpdf["Fiyat"]>tmpdf["ho_long"] 
+    
+    tmpdf["BUY"] =  (tmpdf["BUYs"] == True)  & (tmpdf["BUYs"].rolling(window =2).min() == False)
+    tmpdf["SELL"] =  (tmpdf["SELLs"] == True)  & (tmpdf["SELLs"].rolling(window =2).min() == False)
+    
+    
+    tmpdf["BUY"] = tmpdf["BUY"].astype(int)
+    tmpdf["SELL"] = tmpdf["SELL"].astype(int)
+    
+    tmpdf["UP"] = ( (tmpdf["ho_short"] > tmpdf["ho_middle"] ) & (tmpdf["ho_short"] > tmpdf["ho_long"] ) )
+    tmpdf["DOWN"] = ( (tmpdf["ho_short"] < tmpdf["ho_middle"] )  & (tmpdf["ho_short"] < tmpdf["ho_long"] )  )
+    
+    
+   
+
+    condition1 = tmpdf['Tarih'] >= Baslangic 
+    condition2 = tmpdf['Tarih'] <= Bitis 
+    tmpdf = tmpdf.loc[condition1]
+    tmpdf = tmpdf.loc[condition2]
+    
+    return tmpdf
+    
+ 
+@hatayakala            
+def fonGrafik(fon_kod, df):
+   
+    tmpdf = df.copy()
+    
+    tmpdf["BUY"]=  tmpdf["Fiyat"] * tmpdf["BUY"]
+    tmpdf["SELL"]=  tmpdf["Fiyat"] * tmpdf["SELL"]
+    tmpdf["BUY"].replace(0, np.nan, inplace=True)
+    tmpdf["SELL"].replace(0, np.nan, inplace=True)
+    
+    tmpdf = pd.melt(tmpdf, id_vars=['Tarih', 'BUY','SELL'], value_vars=['Fiyat', 'ho_short', 'ho_middle', 'ho_long'])
+    
+    fig, ax = plt.subplots()
+    
+    sns.lineplot(ax = ax, x ="Tarih", y = "BUY",  data=tmpdf, marker="^", color="green")  
+    sns.lineplot(ax = ax, x ="Tarih", y = "SELL",  data=tmpdf, marker="v", color="red")  
+    
+    sns.lineplot(ax = ax,  x ="Tarih", y = "value", hue="variable", data=tmpdf, linewidth=.80)   # 'AES')
+    plt.xticks(rotation='vertical')
+    
+    #today = date.today()
+    #plt.savefig('./plots/'+ fon_kod + today.strftime("%Y%m%d") + '.png') # önce kayıt etmek şart, sonra kayıt edince olmuyor...
+    
+    return fig
+  
+@hatayakala
 def fonTrend(fon_kod, df):
+
+    tmpdf = df.copy()
     
-    try:
+    tmpdf["Tarih"] = pd.to_datetime(tmpdf["Tarih"], format="%Y.%m.%d")
+    tmpdf.sort_values("Tarih", inplace=True)
     
-        tmpdf = df.copy()
-        
-        tmpdf["Tarih"] = pd.to_datetime(tmpdf["Tarih"], format="%Y.%m.%d")
-        tmpdf.sort_values("Tarih", inplace=True)
-        
-        tmpdf = tmpdf[["Tarih", "Fiyat"]]
-        
-        tmpdf["Fiyat"]= tmpdf["Fiyat"].apply(lambda x: str(x).replace(",","."))
-        tmpdf["Fiyat"] = pd.to_numeric(tmpdf["Fiyat"], downcast="float")
-            
-        tmpdf.set_index('Tarih', inplace=True)
-        
-        
-        res = STL(tmpdf, period=30, ).fit()
-        plt.xticks(rotation='vertical')
-        
-        fig1 = res.plot()
-        
-        
-        
-        return fig1
+    tmpdf = tmpdf[["Tarih", "Fiyat"]]
     
-    except BaseException as e:
-            
-            print("Bir Hata Oluştu : ", e)
+    tmpdf["Fiyat"]= tmpdf["Fiyat"].apply(lambda x: str(x).replace(",","."))
+    tmpdf["Fiyat"] = pd.to_numeric(tmpdf["Fiyat"], downcast="float")
+        
+    tmpdf.set_index('Tarih', inplace=True)
+    
+    
+    res = STL(tmpdf, period=30, ).fit()
+    plt.xticks(rotation='vertical')
+    
+    fig1 = res.plot()
+    
+    
+    
+    return fig1
+    
    
